d828a7c2dfa35d09ed6941d11821182efe2a7bdd
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-16 18:43:26 -0400

    Addes stylesheet

--- marketing-plugin.php
+++ marketing-plugin.php
@@ -9,19 +9,24 @@
  * Version: 1.0.0
  */
 
-
+// Enqueued Style Sheet
+function my_plugin_styles(){
+	wp_enqueue_style('plugin-style', plugins_url('/css/style.css', __FILE__));
+}
+add_action( 'wp_enqueue_scripts', 'my_plugin_styles' );
 
 // Creates Custom Post Type
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
   register_post_type( 'marketing',
     array(
-      'labels' => array(
-        'name' => __( 'Marketing' ),
+      'labels' 			=> array(
+        'name' 			=> __( 'Marketing' ),
         'singular_name' => __( 'Marketing' )
       ),
-      'public' => true,
+      'public' 		=> true,
       'has_archive' => true,
+      'supports' 	=> array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
     )
   );
 }
