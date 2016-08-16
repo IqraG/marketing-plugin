c5af08877958187e1515b9a739bf6b0b8b9d748f
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-16 18:48:46 -0400

    Enqueued Style Sheet
    Forgot to rename function so it gave me a fatal error

--- marketing-plugin.php
+++ marketing-plugin.php
@@ -10,10 +10,10 @@
  */
 
 // Enqueued Style Sheet
-function my_plugin_styles(){
-	wp_enqueue_style('plugin-style', plugins_url('/css/style.css', __FILE__));
+function mrkting_plugin_styles(){
+	wp_enqueue_style('plugin-style', plugins_url('/labfour/style.css', __FILE__));
 }
-add_action( 'wp_enqueue_scripts', 'my_plugin_styles' );
+add_action( 'wp_enqueue_scripts', 'mrkting_plugin_styles' );
 
 // Creates Custom Post Type
 add_action( 'init', 'create_post_type' );
