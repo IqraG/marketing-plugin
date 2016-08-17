9b19d56220d90e15917a01f86040461bf22d5a78
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-16 21:05:29 -0400

    Added Query to Display Custom Posts

--- marketing-plugin.php
+++ marketing-plugin.php
@@ -44,80 +44,64 @@ class MarketingWidget extends WP_Widget{
 		parent::__construct('customposts','Marketing Posts', $widget_ops);
 	}
 
-	// Determins what appears on the site
+	// Determines what appears on the site
 	public function widget($args, $instance){
-		$c = !empty($instance['count']) ? '1' : '0'; // Count Enabled
-		$d = !empty($instance['dropdown']) ? '1' : '0'; // Dropdown Enabled
 		// If there is a title, display title, otherwise display Default Title
 		$title = apply_filters('widget_title', empty($instance['title']) ? 'Marketing Posts' : $instance['title'], $instance, $this->id_base); //apply_filter sanitizes code so no bad code gets inputted
 		echo $args['before_widget'];
 		
 		if($title){
-			// If  dropdown is checked, gets archives and displays them by year 
 			echo $args['before_title'] . $title . $args['after_title'];
-		}
-		// The following code creates a drop-down *just copy this
-		if($d){
-			$dropdown_id = "{$this->id_base}-dropdown-{$this->number}";
-			?>
-			<label class="screen-reader-text" for="<?php echo esc_attr($dropdown_id); ?>"><?php echo $title; ?></label>
-			<select id="<?php echo esc_attr($dropdown_id); ?>" name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'>
-				<?php
-					$dropdown_args = apply_filters('mrkting_dropdown_args', array(
-						'type' => 'yearly',
-						'format' => 'option',
-						'show_post_count' => $c // If post count checked, show the post count
-					)); 
-				?>
-				<option value="Select Year">Select Year</option>
-				<?php wp_get_archives($dropdown_args); ?>
-			</select>
-			
-			<?php } 
-			else { ?>
+		} ?>
 
-			<ul>
-				<?php wp_get_archives(apply_filters('widget_archives_args', array(
-						'type' => 'yearly',
-						'show_post_count' => $c
-					))); // gets a list of the archives and displays them by year. If the Count option is checked, this gets shown.
-					?>
+			<ul id="mrkting-widget">
+			<?php
+			// Custom Query Arguments Setup
+			$cpost_query = new WP_Query(array( 
+				'post_type'		 => 'marketing', 
+				'posts_per_page' => 4
+			 	) );
+			// Displays the custom posts from Marketing. Shows Title and Featured posts
+			if ($cpost_query->have_posts()): while ($cpost_query->have_posts()): $cpost_query->the_post(); ?>		
+				<li class="sidebar-cposts">
+					<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        					<?php the_post_thumbnail(); ?>
+    				</a>
+				</li>				
+				<?php 
+					endwhile;
+					wp_reset_postdata(); // Resets Post Data
+					endif; 
+					wp_reset_query(); // Ends Query
+				?>
 			</ul>
 
-			<?php  }
-				echo $args['after_widget']; // what's set up when you registered the sidebar
-				}
-				// Sets up the form for users to set their options/add content in the widget admin page
-				public function form($instance){
-					$instance = wp_parse_args((array) $instance, array('title'=>'','count'=> 0, 'dropdown'=> ''));
-					$title = strip_tags($instance['title']);
-					$count = $instance['count'] ? 'checked="checked"' : '';
-					$dropdown = $instance['dropdown'] ? 'checked="checked"' : '';
-					?>
-					<p>
-						<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label> 
-						<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-						</p>
-						<p>
-							<input class="checkbox" type="checkbox" <?php echo $dropdown; ?> id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>" /> 
-							<label for="<?php echo $this->get_field_id('dropdown'); ?>">Display as dropdown</label>
-							<br/>
-							<input class="checkbox" type="checkbox" <?php echo $count; ?> id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" /> 
-							<label for="<?php echo $this->get_field_id('count'); ?>">Show post count</label>
-						</p>
+			<?php
+			echo $args['after_widget']; // What comes up when Widget is registered
+	}
 
-			<?php }
-			// Save, Submit, Sanitize 
-			public function update($new_instance,$old_instance){
-				$instance = $old_instance;
-				$new_instance = wp_parse_args((array) $new_instance, array('title' => '', 'count' => 0, 'dropdown' => ''));
-				$instance['title'] = strip_tags($new_instance['title']);
-				$instance['count'] = $new_instance['count'] ? 1 : 0;
-				$instance['dropdown'] = $new_instance['dropdown'] ? 1 : 0;
-				return $instance;
-			}			
+	// Determines what appears on the dashboard. Allows users to control what appears on the website
+	public function form($instance){
+		$instance = wp_parse_args((array) $instance, array('title'=>''));
+		$title = strip_tags($instance['title']);
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+		</p>
+		<?php 
+	}
+
+	// Save, Submit, Sanitize 
+	public function update($new_instance,$old_instance){
+		$instance = $old_instance;
+		$new_instance = wp_parse_args((array) $new_instance, array('title' => '', 'count' => 0, 'dropdown' => ''));
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
+	}			
 }
-add_action('widgets_init',function(){ register_widget('YearlyArchives'); });
+add_action('widgets_init', function(){ register_widget('MarketingWidget'); });
 
 
 
